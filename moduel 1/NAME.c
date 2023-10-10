a80b1948f9337323b579f53f37096b694bb309d2
Himanshu2702P <146431383+Himanshu2702P@users.noreply.github.com>
2023-10-10 11:29:27 +0530

    Add files via upload

---
+++ moduel 1/NAME.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("Name\tPANCHAL HIMANSHU\n");
+	printf("DOB\t27/02/1999\n");
+	printf("Age\t 25\n");
+	printf("Address\t Dev aashish-2,\n\t Nava Naroda\n\t ahemdabad");
+	return 0;
+}
